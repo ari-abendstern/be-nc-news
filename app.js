7f234139e292e5649a1df8f0bc2d0a9b98053f71
Ari Abendstern <ari.abendstern@gmx.com>
2023-11-20 15:51:21 +0000

    create /api/articles/:article_id endpoints&errors

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
 const {
-  getTopics,
+  getTopics, getArticleById,
   
 } = require("./controllers/index.controllers");
 const {
   
   handleServerErrors,
   handleNonExistentPath,
+  handleCustomErrors,
+  handlePsql22P02,
 } = require("./errors/index");
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById)
+
 app.all("/api/*", handleNonExistentPath);
 
+app.use(handleCustomErrors)
+
+app.use(handlePsql22P02)
 
 app.use(handleServerErrors);
 
