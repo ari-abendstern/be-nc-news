6c85f09dd4501ee6111094ae79bfe8936a459734
Ari Abendstern <ari.abendstern@gmx.com>
2023-11-25 20:40:03 +0000

    improve formatting to increase readability

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,9 +1,9 @@
 const apiRouter = require("express").Router();
 const { getEndpoints } = require("../controllers/index.controllers");
-const articlesRouter = require("./articles-router");
-const commentsRouter = require("./comments-router");
 const topicsRouter = require("./topics-router");
 const usersRouter = require("./users-router");
+const commentsRouter = require("./comments-router");
+const articlesRouter = require("./articles-router");
 
 apiRouter.route("/").get(getEndpoints);
 
